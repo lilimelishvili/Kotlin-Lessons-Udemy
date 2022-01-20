1bb524da848667aab212a970f0aaf06b139e2e06
lilimelishvili <lilimerlishvili@gmail.com>
2022-01-20 22:57:40 +0400

    Kotlin Udemy || Kotlin Fundamentals - Exercise Variables And Datatypes

--- kotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ kotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,42 +1,31 @@
 package com.example.kotlinbasics
-
-fun main () {
-    // immutable variable
-    // TODO: Add new functionality
-    /*
-    This
-    is a multiline
-    comment
-     */
-    // type string
-    var myName = "Lily"
-    // type int 32 bit
-    // type inference
-    var myAge = 32
-    // Integer TYPES: Byte ( 8 bit ) Short ( 16 bit )  Int  ( 32 bit )  Long ( 64 bit )
-    val myByte: Byte = 13
-    val myShot: Short = 125
-    val myInt:  Int = 123123123
-    val myLong: Long = 39_812_309_487_120_300
-
-
-    // Floating Point number Types :  Float ( 32 bit), Double ( 64 bit )
-    val myFloat = 13.37F
-    val myDouble = 3.1459265358979323846
-
-    //Boolean is a type which represents logical values.
-    // It can have two possible values, true and false.
-    var isSunny: Boolean = true
-    isSunny = false
-    //Characters
-    var letterChar = 'A'
-    var digitChar = '1'
-
-    // Strings
-    val myStr = "Hello World"
-    var firstCharInStr = myStr [0]
-    var lastCharInStr = myStr[myStr.length - 1]
-
-
-    print ("Last character " + lastCharInStr)
+/*
+Exercise Variables And Datatypes
+Please write a program in which you are assigning those values to variables with the right (specific) data type:
+"Android Masterclass"
+13.37F
+3.14159265358979
+25
+2020
+18881234567
+true
+'a'
+Please don't simply use Kotlin's powerful type inference feature for this.
+For example, write
+var age : Short = 25
+and not
+var age = 25
+even though both are correct. This is just for testing your understanding of the different basic data types.
+All of them should be "var", except for the pi value and 13.37, use "val" here.
+Try to use descriptive and useful names
+ */
+fun main(){
+    var courseName :String = "Android Masterclass"
+    val leet : Float = 13.37F
+    val pi : Double = 3.14159265358979
+    var age : Byte = 25
+    var year : Short = 2020
+    var phoneNumber: Long = 18881234567
+    var isGood : Boolean = true
+    var firstCharacter : Char = 'a'
 }
