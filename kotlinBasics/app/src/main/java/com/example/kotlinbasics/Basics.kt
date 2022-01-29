634218fde9f2d25be724e67d4ef27fb45ba0951a
lilimelishvili <lilimerlishvili@gmail.com>
2022-01-29 20:46:31 +0400

    Function_Parameters_Argumens_Return

--- kotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ kotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,14 +1,22 @@
 package com.example.kotlinbasics
 
-fun main() {
 
-    for (i in  1 until 20){
-// 10/2 = 5
-    // 11/2 = 5.5 which is  5 in term of an  int
-        if (i/2 == 5){
-            continue
-        }
-        print("$i ")
-    }
-    print("done with the loop")
+
+fun main() {
+    ///Method - a Method is a Function within a class
+    //argument
+    var avg = avg( a =  5.3, b = 13.37 )
+   print("result is  $avg")
+}
+
+fun avg(a: Double, b: Double): Double{
+    return (a+b)/2
+}
+// Parameter (input)
+fun addUp(a: Int, b: Int) : Int{
+    // (output)
+    return a+b
+}
+fun myFunction  (){
+    print("Called from myFuncion ")
 }
