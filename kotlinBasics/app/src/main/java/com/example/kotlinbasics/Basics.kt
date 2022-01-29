cebd6e09b04096fa56c009a17e46e60b4c37eedd
lilimelishvili <lilimerlishvili@gmail.com>
2022-01-29 19:08:47 +0400

    Break_and_Continue

--- kotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ kotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,49 +1,14 @@
 package com.example.kotlinbasics
 
-fun main () {
-     var  x  = 100
-    while (x >= 0 ){
-        print("$x")
-        x -= 2
-    }
-        println("\n while loop is done")
+fun main() {
 
-     x = 1
-     do{
-         println("$x")
-        x++
-     }while (x <= 10)
-    println("\ndo while loop is done")
-
-    var feltTemp = "cold"
-    var roomTemp = 10
-    while (feltTemp == "cold"){
-        roomTemp ++
-        if (roomTemp >= 20){
-            feltTemp = "compy"
-            println("It's compy now")
+    for (i in  1 until 20){
+// 10/2 = 5
+    // 11/2 = 5.5 which is  5 in term of an  int
+        if (i/2 == 5){
+            continue
         }
-    }
-     for (num in 1..10) {
-        print("$num") }
-    for (i in 1 until 10){ // Same as - for (i in 1.until(10))
         print("$i ")
     }
-     println("____________")
-    for (i in 10 downTo  1 step 2){// Same as - for (i in 10 downTo(1).step(2))
-        print("$i ")
-    }
-
-
-
-
-
+    print("done with the loop")
 }
-
-
-
-
-
-
-
-
