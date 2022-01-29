505408226b5f40b444661df03b462f9a7e42132e
lilimelishvili <lilimerlishvili@gmail.com>
2022-01-29 21:40:21 +0400

    Nillable

--- kotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ kotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -2,21 +2,32 @@ package com.example.kotlinbasics
 
 
 
-fun main() {
-    ///Method - a Method is a Function within a class
-    //argument
-    var avg = avg( a =  5.3, b = 13.37 )
-   print("result is  $avg")
+
+fun main(){
+    //var name :String = "Lily"
+    //name=null -> Compilation ERROR
+    var nullableName: String? = "Lily"
+    //null.also { nullableName = it }
+
+
+    var  len = nullableName?.length
+    nullableName?.let { println(it.length) }
+
+
+    // ?: Elvis operator
+    val name = nullableName  ?: "Guest"
+    println("name is $name")
+
+    nullableName!!.toLowerCase()
+
+    /*if (nullableName != null){
+        var  len2 = nullableName.length
+
+    }else{
+        null
+    }*/
 }
 
-fun avg(a: Double, b: Double): Double{
-    return (a+b)/2
-}
-// Parameter (input)
-fun addUp(a: Int, b: Int) : Int{
-    // (output)
-    return a+b
-}
-fun myFunction  (){
-    print("Called from myFuncion ")
+private fun String.forEach() {
+    TODO("Not yet implemented")
 }
