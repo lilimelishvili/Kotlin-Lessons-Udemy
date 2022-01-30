c0e7e34a09ea7609b6f22afcb01fe5859ba0dc3d
lilimelishvili <lilimerlishvili@gmail.com>
2022-01-30 23:38:09 +0400

    Member_Variables_Functions_And_Constructors

--- kotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ kotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,13 +1,44 @@
 package com.example.kotlinbasics
 
-fun main(){
-    val iphone = MobilePhone("iOS", "Apple", "iPhone 12")
-    val galaxyS20 = MobilePhone("Android","Samsung", "Galaxy S20")
-    val mateXS = MobilePhone("Android", "Huawei", "Mate X S")
+fun main() {
+    //Creating instances of classes
+  var denis = Person("Lily", "Ime", 31)
+    denis.hobby = "tu skateboard"
+    denis.age = 32
+    println("Lily is ${denis.age} years old" )
+    denis.stateHobby()
+    var Gio = Person ()
+    Gio.hobby = "play video games"
+    Gio.stateHobby()
+   // var GioEpita = Person (lastName = "Epita")
 }
 
-class MobilePhone(osName: String, brand: String, model: String){
+class Person(firstName: String = "Gio", lastName: String = "Epo" ){
+     // Member Variable - Properties
+    var age : Int? = null
+    var hobby : String = "Watch Netflix"
+    var firstName : String? = null
+
+    //initializer Block
     init {
-        println("The phone $model from $brand uses $osName as its Operating System")
+        this.firstName = firstName
+        println("Initialized a new Person object with " +
+                "firstName = $firstName, lastName = $lastName")
+
     }
+
+    // Member seconary Constructor
+
+    constructor(firstName: String, lastName: String, age: Int): this(firstName, lastName){
+        this.age = age
+    }
+
+
+
+    // Member functins - Methods
+    fun stateHobby(){
+        println("$firstName\'s  hobby is $hobby")
+    }
+
+
 }
