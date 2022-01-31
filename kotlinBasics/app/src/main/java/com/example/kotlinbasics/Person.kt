fa54bf9fd4d476d91078084648cbc2b657f916e4
lilimelishvili <lilimerlishvili@gmail.com>
2022-02-01 00:06:51 +0400

    Inheritance

--- kotlinBasics/app/src/main/java/com/example/kotlinbasics/Person.kt
+++ kotlinBasics/app/src/main/java/com/example/kotlinbasics/Person.kt
@@ -1,21 +1,47 @@
 package com.example.kotlinbasics
 
-fun main(){
-    val iphone = MobilePhone("iOS", "Apple", "iPhone 12")
-    val galaxyS20 = MobilePhone("Android","Samsung", "Galaxy S20")
-    val mateXS = MobilePhone("Android", "Huawei", "Mate X S")
+//Super Class, Parent Class, Base Class
+open class Vehicle{
+    //properties
+    //methods
 }
+//Sub Class Child Class or derived class of Vehicle
+//Super Class, Parent Class, Base Class of  Vehicle
+open class Car(val name: String, val brand: String){
 
-class MobilePhone(osName: String, brand: String, model: String){
-
-    private var battery = 30
-
-    init {
-        println("The phone $model from $brand uses $osName as its Operating System")
+open var range: Double = 0.0
+    fun  extendRange(amount: Double) {
+        if (amount > 0)
+            range += amount
+    }
+   open fun drive(distance: Double){
+        println("drove for $distance KM")
     }
 
-    fun chargeBattery(chargedBy: Int){
-        println("Battery was at $battery and is at ${battery+chargedBy} now")
-        battery += chargedBy
+
+}
+//Sub Class Child Class or derived class of Car
+
+class ElectricCar(name: String, brand: String, batteryLife: Double  ): Car(name, brand){
+
+    var chargerType ="Type1"
+    override var range = batteryLife * 6
+    override fun drive(distance: Double) {
+        println("drove for $distance KM on elecriciy")
     }
+fun drive(){
+    println("drove for $range KM on elecriciy")
+}
+}
+fun main(){
+    var audiA3 = Car(  "A3", "Audi")
+    var teslaS = ElectricCar("S-Model", "Tesla", 85.0)
+    teslaS.extendRange(200.0)
+    teslaS.chargerType = "Type2"
+
+    teslaS.drive()
+    // polymorphism
+    teslaS.drive(200.0)
+    audiA3.drive(200.0)
+
 }
