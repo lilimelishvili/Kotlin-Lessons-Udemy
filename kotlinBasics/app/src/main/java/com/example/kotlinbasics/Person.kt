ff3fde4a30de81eeb6f4dc24f3cb7a87efaa66b8
lilimelishvili <lilimerlishvili@gmail.com>
2022-02-11 21:39:18 +0400

    Lists

--- kotlinBasics/app/src/main/java/com/example/kotlinbasics/Person.kt
+++ kotlinBasics/app/src/main/java/com/example/kotlinbasics/Person.kt
@@ -1,23 +1,29 @@
 package com.example.kotlinbasics
 
 fun main() {
-//val nymbers:IntArray = IntArray(1,2,3,4,5,6,)
-    //   val numbers = intArrayOf(1,2,3,4,5,6,)
-    val numbers = arrayOf(1, 2, 3, 4, 5, 6,)
-    // print(numbers.contentToString())
-    val numbersD:DoubleArray = doubleArrayOf(1.0,2.0,3.0,4.0,5.0,6.0)
-    print("initial value ${numbers.contentToString()}")
-    numbers[0] = 6
-    numbers[1] = 5
-    numbers[4] = 2
-    numbers[5] = 1
-    print("\nfinal value ${numbers.contentToString()}")
-    val days = arrayOf("Sun","Mon", "tues", "Wed", "thurs","Fri","Sat")
-    print(days.contentToString())
-  val fruits = arrayOf(Fruit(name = "Apple", price = "2.5"), Fruit(name = "Grape", price = "2.1"  ))
-for (index in fruits.indices){
-    print("${fruits[index].name} is in index $index")
-}
-}
-data class Fruit(val name:String, val price: String)
+    val months = listOf("Januari", "Fabruary", "March")
+    val anyTypes = listOf(1,2,3,true,false,"string")
+    //print(anyTypes.size)
+   // print(months[1])
 
+    /*for (month in months){
+        println(month)
+    }*/
+    val adionalMonths = months.toMutableList()
+    val newMonths = arrayOf("April", "May", "June")
+    adionalMonths.addAll(newMonths)
+    adionalMonths.add("July")
+    //print(adionalMonths)
+
+
+    val days = mutableListOf<String>("Mon", "Tue", "Wed")
+    days.add("Thu")
+    days[2] = "Sunday"
+    //days.removeAt(3)
+    val removelist = mutableListOf<String>("Mon", "Wed")
+    //days.removeAll(removelist)
+    days.removeAll(days)
+    print(days)
+
+
+}
