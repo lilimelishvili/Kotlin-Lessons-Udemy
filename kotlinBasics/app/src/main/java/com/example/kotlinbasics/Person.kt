e6fdc751f8445afead594d0ddcdcab5adcbe0195
lilimelishvili <lilimerlishvili@gmail.com>
2022-02-11 22:17:41 +0400

    Sets_and_Maps

--- kotlinBasics/app/src/main/java/com/example/kotlinbasics/Person.kt
+++ kotlinBasics/app/src/main/java/com/example/kotlinbasics/Person.kt
@@ -1,29 +1,27 @@
 package com.example.kotlinbasics
 
 fun main() {
-    val months = listOf("Januari", "Fabruary", "March")
-    val anyTypes = listOf(1,2,3,true,false,"string")
-    //print(anyTypes.size)
-   // print(months[1])
+    val fruits = setOf("Orange", "Apple", "Mango", "Grape", "Apple")
+    //print(fruits.toSortedSet())
 
-    /*for (month in months){
-        println(month)
-    }*/
-    val adionalMonths = months.toMutableList()
-    val newMonths = arrayOf("April", "May", "June")
-    adionalMonths.addAll(newMonths)
-    adionalMonths.add("July")
-    //print(adionalMonths)
+    val newFruits = fruits.toMutableList()
+    newFruits.add("Water Melon")
+    newFruits.add("Pear")
+    //println(newFruits.elementAt(4))
 
+    val daysOFTheWeek = mapOf(1 to "Monday",   2 to "Tuesday",    3 to "Wednesday")
+    println(daysOFTheWeek[2])
 
-    val days = mutableListOf<String>("Mon", "Tue", "Wed")
-    days.add("Thu")
-    days[2] = "Sunday"
-    //days.removeAt(3)
-    val removelist = mutableListOf<String>("Mon", "Wed")
-    //days.removeAll(removelist)
-    days.removeAll(days)
-    print(days)
-
-
+    for (key in daysOFTheWeek.keys) {
+        print("$key is to ${daysOFTheWeek[key]}")
+    }
+val fruitsMap = mapOf("Favorite" to Fruit("Grape",2.5),
+    "OK" to Fruit("Appl", 1.0) )
+    val newDaysOfWeek = daysOFTheWeek.toMutableMap()
+    newDaysOfWeek[4] = "Thursday"
+    newDaysOfWeek[5] = "Friday"
+ print(newDaysOfWeek.toSortedMap())
 }
+
+data class  Fruit(val name: String, val price: Double)
+
