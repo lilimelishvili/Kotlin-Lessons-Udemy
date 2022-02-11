44e92aa1fd949893563eb011e677d50c21a551ad
lilimelishvili <lilimerlishvili@gmail.com>
2022-02-11 22:33:41 +0400

    ArrayLists

--- kotlinBasics/app/src/main/java/com/example/kotlinbasics/Person.kt
+++ kotlinBasics/app/src/main/java/com/example/kotlinbasics/Person.kt
@@ -1,27 +1,13 @@
 package com.example.kotlinbasics
 
-fun main() {
-    val fruits = setOf("Orange", "Apple", "Mango", "Grape", "Apple")
-    //print(fruits.toSortedSet())
-
-    val newFruits = fruits.toMutableList()
-    newFruits.add("Water Melon")
-    newFruits.add("Pear")
-    //println(newFruits.elementAt(4))
-
-    val daysOFTheWeek = mapOf(1 to "Monday",   2 to "Tuesday",    3 to "Wednesday")
-    println(daysOFTheWeek[2])
-
-    for (key in daysOFTheWeek.keys) {
-        print("$key is to ${daysOFTheWeek[key]}")
+fun main(args:Array<String>) {
+    val arrayList:ArrayList<String> = ArrayList<String>()
+    arrayList.add("One")
+    arrayList.add("Two")
+    for (i in arrayList){
+        println(i)
     }
-val fruitsMap = mapOf("Favorite" to Fruit("Grape",2.5),
-    "OK" to Fruit("Appl", 1.0) )
-    val newDaysOfWeek = daysOFTheWeek.toMutableMap()
-    newDaysOfWeek[4] = "Thursday"
-    newDaysOfWeek[5] = "Friday"
- print(newDaysOfWeek.toSortedMap())
+    println("....arrayList.get(1).....")
+    println(arrayList.get(1))
+
 }
-
-data class  Fruit(val name: String, val price: Double)
-
