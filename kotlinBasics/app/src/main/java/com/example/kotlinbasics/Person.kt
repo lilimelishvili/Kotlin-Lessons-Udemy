0ab43b7afacf5ec1befd26ffaeab2da6be31a38f
lilimelishvili <lilimerlishvili@gmail.com>
2022-02-16 22:23:22 +0400

    Inner Classes

--- kotlinBasics/app/src/main/java/com/example/kotlinbasics/Person.kt
+++ kotlinBasics/app/src/main/java/com/example/kotlinbasics/Person.kt
@@ -4,13 +4,18 @@ class OuterClass {
     //outerclass code
     private var name: String = "MR X"
 
-    class NestedClass {
-        //nestedclass code
-        var descrption: String = "code inside nested class"
-        private var id: Int = 101
-        fun foo() {
-            //pint("name is ${name}") //cannot access the outer class member
+    inner class InnerClass{
+       // Inner Class code
+        var description:String="code inside inner class"
+        private var id:Int = 101
+        fun foo(){
+            println("name is ${name}")// access the private outer class member
             println("Id is ${id}")
         }
     }
+       }
+fun main(args:Array<String>){
+    println(OuterClass().InnerClass().description)//accessing property
+    var obj = OuterClass().InnerClass()//object creation
+    obj.foo()//access member function
 }
