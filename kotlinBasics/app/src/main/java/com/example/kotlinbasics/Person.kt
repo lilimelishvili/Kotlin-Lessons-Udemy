15166e272cadd37820653c2d14df6b51dd0f6efb
lilimelishvili <lilimerlishvili@gmail.com>
2022-02-16 22:32:17 +0400

    Safe Cast and Unsafe Cast Operator

--- kotlinBasics/app/src/main/java/com/example/kotlinbasics/Person.kt
+++ kotlinBasics/app/src/main/java/com/example/kotlinbasics/Person.kt
@@ -1,21 +1,9 @@
 package com.example.kotlinbasics
 
-class OuterClass {
-    //outerclass code
-    private var name: String = "MR X"
-
-    inner class InnerClass{
-       // Inner Class code
-        var description:String="code inside inner class"
-        private var id:Int = 101
-        fun foo(){
-            println("name is ${name}")// access the private outer class member
-            println("Id is ${id}")
-        }
-    }
-       }
 fun main(args:Array<String>){
-    println(OuterClass().InnerClass().description)//accessing property
-    var obj = OuterClass().InnerClass()//object creation
-    obj.foo()//access member function
+    var location: Any= "Kotlin"
+    var safeString:String?=location as? String
+    var safeInt:Int?=location as? Int
+    println(safeString)
+    println(safeInt)
 }
